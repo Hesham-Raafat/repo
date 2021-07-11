13535c4cbda14b34a21d21e0c03e5b89653afbbe
Hesham-Raafat <42641731+Hesham-Raafat@users.noreply.github.com>
2021-07-11 02:24:11 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,12 +8,10 @@
 dockerpath="heshamraafat/python:v1"
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run mypod --image=$dockerpath --port=80 --labels="mypod"
-
+kubectl run mypod --image=$dockerpath --port=80
 # Step 3:
 # List kubernetes pods
 kubectl get pods -o wide
 # Step 4:
 # Forward the container port to a host
-export mypod=$(kubectl get pod -l mypod -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $mypod 8000:80
+kubectl port-forward pod/mypod 8000:80
